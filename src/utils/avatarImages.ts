fafc24feff418b067d6fa4d704d4057d8953b52f
Limax <limax.workspace@gmail.com>
2025-06-11 18:28:43 +0200

    feat: add avatar images and options for user profile customization

---
+++ src/utils/avatarImages.ts
@@ -0,0 +1,56 @@
+export const avatarOptions = [
+  {
+    id: 1,
+    source: require("@/assets/avatar-1.png"),
+    name: "Orange",
+    key: "avatar_1",
+  },
+  {
+    id: 2,
+    source: require("@/assets/avatar-2.png"),
+    name: "Blue",
+    key: "avatar_2",
+  },
+  {
+    id: 3,
+    source: require("@/assets/orange.png"),
+    name: "Green",
+    key: "avatar_3",
+  },
+  {
+    id: 4,
+    source: require("@/assets/orange.png"),
+    name: "Purple",
+    key: "avatar_4",
+  },
+  {
+    id: 5,
+    source: require("@/assets/orange.png"),
+    name: "Red",
+    key: "avatar_5",
+  },
+  {
+    id: 6,
+    source: require("@/assets/orange.png"),
+    name: "Yellow",
+    key: "avatar_6",
+  },
+  {
+    id: 7,
+    source: require("@/assets/orange.png"),
+    name: "Pink",
+    key: "avatar_7",
+  },
+  {
+    id: 8,
+    source: require("@/assets/orange.png"),
+    name: "Teal",
+    key: "avatar_8",
+  },
+  {
+    id: 9,
+    source: require("@/assets/orange.png"),
+    name: "Indigo",
+    key: "avatar_9",
+  },
+];
